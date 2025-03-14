f21178d8baa0b00dc6f6228204cb403f8ac5e537
djarae <51816647+djarae@users.noreply.github.com>
2025-03-14 16:59:00 -0300

    avances simulacion

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express'); const path = require('path'); const app = express(); const port = process.env.PORT || 7777;
+const cors = require("cors");
 app.get('/', (req, res) => { res.send('Hello World!Funciona el despliegue 9fj39r') })
 app.listen(port, () => { console.log(`Example app listening on port ${port}`) })
+// 🔥 Agregar CORS correctamente antes de definir rutas
+app.use(cors({ origin: "http://localhost:3001", credentials: true }));
+
+// Middleware para parsear JSON
+app.use(express.json());
+
 
 app.get('/OficinaInternet/servlet/docs', (req, res) => {
   const file = req.query.file; // Se obtiene el archivo de la URL
@@ -17,4 +24,16 @@ app.get('/OficinaInternet/servlet/docs', (req, res) => {
       res.status(500).send('Error al recuperar el archivo.');
     }
   });
+});
+
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Usuario y contraseña requeridos" });
+  }
+  console.log("Usuario:", username);
+  console.log("Contraseña:", password);
+  // Aquí podrías hacer validaciones con una base de datos en el futuro
+  res.json({ message: "Login exitoso", username });
 });
